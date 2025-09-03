04b1a326dbf552b61165e7d2140ea089f2a3c3d9
adityakarmokar2000@gmail.com <adityakarmokar2000@gmail.com>
2025-09-03 22:43:51 +0530

    changes

--- resources/views/web/booking/partials/cruise.blade.php
+++ resources/views/web/booking/partials/cruise.blade.php
@@ -1,7 +1,7 @@
 <!------------------------ Cruise Booking Details ------------------------------>
 <div class="tab-pane fade" id="cruisebooking" role="tabpanel" aria-labelledby="cruisebooking-tab">
     <div class="card p-4 show-booking-card">
-        
+
         <div class="position-relative checkbox-servis mb-2 mt-2">
             <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" name="payment_type" id="fullPayment"
@@ -14,7 +14,7 @@
                 <label class="form-check-label" for="deposit">Deposit</label>
             </div>
         </div>
-        
+
 
         <h5 class="card-header border-0 p-0 mb-0 detail-passanger">Cruise Booking Details</h5>
 
@@ -34,21 +34,21 @@
                     </thead>
                     <tr>
                         <td> <input type="text" class="form-control" placeholder="Cruise Line" name="cruise_line"
-                                value="{{$travel_cruise_data?->cruise_line}}"></td>
+                                value="{{ $travel_cruise_data?->cruise_line }}"></td>
                         <td><input type="text" class="form-control" placeholder="Name of the Ship"
                                 placeholder="Name of the Ship" name="ship_name"
-                                value="{{$travel_cruise_data?->ship_name}}">
+                                value="{{ $travel_cruise_data?->ship_name }}">
                         </td>
                         <td><input type="text" class="form-control" placeholder="Cruise Trip Name" name="cruise_name"
-                                value="{{$travel_cruise_data?->cruise_name}}"></td>
+                                value="{{ $travel_cruise_data?->cruise_name }}"></td>
                         <td> <input type="text" class="form-control" placeholder="Length" placeholder="length"
-                                name="length" value="{{$travel_cruise_data?->length}}"></td>
-                        <td><input type="text" class="form-control" placeholder="Departure Port" name="departure_port"
-                                value="{{$travel_cruise_data?->departure_port}}"></td>
+                                name="length" value="{{ $travel_cruise_data?->length }}"></td>
+                        <td><input type="text" class="form-control" placeholder="Departure Port"
+                                name="departure_port" value="{{ $travel_cruise_data?->departure_port }}"></td>
                         <td> <input type="text" class="form-control" placeholder="Arival Port" name="arrival_port"
-                                value="{{$travel_cruise_data?->arrival_port}}"></td>
+                                value="{{ $travel_cruise_data?->arrival_port }}"></td>
                         <td><input type="text" class="form-control" placeholder="Stateroom" name="stateroom"
-                                value="{{$travel_cruise_data?->stateroom}}"></td>
+                                value="{{ $travel_cruise_data?->stateroom }}"></td>
                     </tr>
                 </table>
             </div>
@@ -80,36 +80,38 @@
                             </tr>
                         </thead>
                         <tbody id="cruiseForms">
-                            @foreach($booking->travelCruise as $key=>$travelCruise)
-                            <tr class="cruise-row" data-index="{{$key}}">
-                                <td><span class="cruise-title">{{$key+1}}</span></td>
+                            @foreach ($booking->travelCruise as $key => $travelCruise)
+                                <tr class="cruise-row" data-index="{{ $key }}">
+                                    <td><span class="cruise-title">{{ $key + 1 }}</span></td>
 
-                                <td><input style="width: 125px;" type="date" class="form-control"
-                                        name="cruise[{{$key}}][departure_date]"
-                                        value="{{$travelCruise->departure_date?->format('Y-m-d')}}">
-                                </td>
-                                <td><input type="text" class="form-control" style="width:39.5rem"
-                                        name="cruise[{{$key}}][departure_port]"
-                                        value="{{$travelCruise->departure_port}}" placeholder="Port"></td>
+                                    <td><input style="width: 125px;" type="date" class="form-control"
+                                            name="cruise[{{ $key }}][departure_date]"
+                                            value="{{ $travelCruise->departure_date?->format('Y-m-d') }}">
+                                    </td>
+                                    <td><input type="text" class="form-control" style="width:39.5rem"
+                                            name="cruise[{{ $key }}][departure_port]"
+                                            value="{{ $travelCruise->departure_port }}" placeholder="Port"></td>
 
-                                <td><input type="text" class="form-control time_24_hrs" style="width:50px"
-                                        name="cruise[{{$key}}][departure_hrs]" value="{{$travelCruise->departure_hrs}}"
-                                        placeholder="Hrs" min="0" max="23"></td>
-
-
-
-                                <td><input type="text" class="form-control time_24_hrs" style="width:50px;"
-                                        name="cruise[{{$key}}][arrival_hrs]" value="{{$travelCruise->arrival_hrs}}"
-                                        placeholder="Hrs" min="0" max="23"></td>
+                                    <td><input type="text" class="form-control time_24_hrs" style="width:50px"
+                                            name="cruise[{{ $key }}][departure_hrs]"
+                                            value="{{ $travelCruise->departure_hrs }}" placeholder="Hrs" min="0"
+                                            max="23"></td>
 
 
 
-                                <td>
-                                    <button type="button" class="btn btn-outline-danger delete-cruise-btn">
-                                        <i class="ri ri-delete-bin-line"></i>
-                                    </button>
-                                </td>
-                            </tr>
+                                    <td><input type="text" class="form-control time_24_hrs" style="width:50px;"
+                                            name="cruise[{{ $key }}][arrival_hrs]"
+                                            value="{{ $travelCruise->arrival_hrs }}" placeholder="Hrs"
+                                            min="0" max="23"></td>
+
+
+
+                                    <td>
+                                        <button type="button" class="btn btn-outline-danger delete-cruise-btn">
+                                            <i class="ri ri-delete-bin-line"></i>
+                                        </button>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
@@ -117,67 +119,90 @@
             </div>
         </div>
 
-    <!------------ Start Addons ------------------------------>        
-    <div class="mb-0 d-flex align-items-center justify-content-between">
-    <h5 class="card-header border-0 p-0 mb-0 detail-passanger">Cruise Addons</h5>
-    <div class="d-flex gap-2">
+        <!------------ Start Addons ------------------------------>
+        <div class="mb-0 d-flex align-items-center justify-content-between">
+            <h5 class="card-header border-0 p-0 mb-0 detail-passanger">Cruise Addons</h5>
+            <div class="d-flex gap-2">
 
-        <button class="btn btn-primary no-btn add-no-btn add-bank" type="button" id="cruise-addon-button">
+                <button class="btn btn-primary no-btn add-no-btn add-bank" type="button" id="cruise-addon-button">
                     <i class="ri ri-add-circle-fill pointer"></i>
                 </button>
 
-    </div>
-</div>
+            </div>
+        </div>
 
-<div class="row">
-    <div class="col-md-12 table-responsive details-table-wrappper">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Services</th>
-                    <th>Name of Service</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody id="cruise-addon-container">
-                @foreach($travel_cruise_addon as $key => $addon)
-                    <tr class="cruise-addon-row">
-                        <td style="width: 150px">
-                            <select class="form-control" name="cruiseaddon[{{ $key }}][services]">
-                                <option value="">Select</option>
-                                <option value="Excursions" {{ $addon->services == 'Excursions' ? 'selected' : '' }}>Excursions</option>
-                                <option value="WiFi Packages" {{ $addon->services == 'WiFi Packages' ? 'selected' : '' }}>WiFi Packages</option>
-                                <option value="Crew Appreciation Fees/Gratuities" {{ $addon->services == 'Crew Appreciation Fees/Gratuities' ? 'selected' : '' }}>Crew Appreciation Fees/Gratuities</option>
-                                <option value="Shuttle Services" {{ $addon->services == 'Shuttle Services' ? 'selected' : '' }}>Shuttle Services</option>
-                                <option value="Speciality Dining" {{ $addon->services == 'Speciality Dining' ? 'selected' : '' }}>Speciality Dining</option>
-                                <option value="Drink Packages" {{ $addon->services == 'Drink Packages' ? 'selected' : '' }}>Drink Packages</option>
-                                <option value="Trip Insurance" {{ $addon->services == 'Trip Insurance' ? 'selected' : '' }}>Trip Insurance</option>
-                                <option value="Check-in Proces (Luggage Tags & Sailing Pass)" {{ $addon->services == 'Check-in Proces (Luggage Tags & Sailing Pass)' ? 'selected' : '' }}>Check-in Proces (Luggage Tags & Sailing Pass)</option>
-                                <option value="Special Occasion Package" {{ $addon->services == 'Special Occasion Package' ? 'selected' : '' }}>Special Occasion Package</option>
-                                <option value="Water Bottle or Distilled Water Package" {{ $addon->services == 'Water Bottle or Distilled Water Package' ? 'selected' : '' }}>Water Bottle or Distilled Water Package</option>
-                                <option value="Old Itinerary">Old Itinerary</option>
-                                <option value="Changed Itinerary" {{ $addon->services == 'Changed Itinerary' ? 'selected' : '' }}>Changed Itinerary</option>
-                            </select>
-                        </td>
-                        <td style="width:1200px">
-                            <textarea class="form-control ckeditor" name="cruiseaddon[{{ $key }}][service_name]" rows="6">{{ $addon->service_name }}</textarea>
-                        </td>
+        <div class="row">
+            <div class="col-md-12 table-responsive details-table-wrappper">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Services</th>
+                            <th>Name of Service</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="cruise-addon-container">
+                        @foreach ($travel_cruise_addon as $key => $addon)
+                            <tr class="cruise-addon-row">
+                                <td style="width: 150px">
+                                    <select class="form-control" name="cruiseaddon[{{ $key }}][services]">
+                                        <option value="">Select</option>
+                                        <option value="Excursions"
+                                            {{ $addon->services == 'Excursions' ? 'selected' : '' }}>Excursions
+                                        </option>
+                                        <option value="WiFi Packages"
+                                            {{ $addon->services == 'WiFi Packages' ? 'selected' : '' }}>WiFi Packages
+                                        </option>
+                                        <option value="Crew Appreciation Fees/Gratuities"
+                                            {{ $addon->services == 'Crew Appreciation Fees/Gratuities' ? 'selected' : '' }}>
+                                            Crew Appreciation Fees/Gratuities</option>
+                                        <option value="Shuttle Services"
+                                            {{ $addon->services == 'Shuttle Services' ? 'selected' : '' }}>Shuttle
+                                            Services</option>
+                                        <option value="Speciality Dining"
+                                            {{ $addon->services == 'Speciality Dining' ? 'selected' : '' }}>Speciality
+                                            Dining</option>
+                                        <option value="Drink Packages"
+                                            {{ $addon->services == 'Drink Packages' ? 'selected' : '' }}>Drink Packages
+                                        </option>
+                                        <option value="Trip Insurance"
+                                            {{ $addon->services == 'Trip Insurance' ? 'selected' : '' }}>Trip Insurance
+                                        </option>
+                                        <option value="Check-in Proces (Luggage Tags & Sailing Pass)"
+                                            {{ $addon->services == 'Check-in Proces (Luggage Tags & Sailing Pass)' ? 'selected' : '' }}>
+                                            Check-in Proces (Luggage Tags & Sailing Pass)</option>
+                                        <option value="Special Occasion Package"
+                                            {{ $addon->services == 'Special Occasion Package' ? 'selected' : '' }}>
+                                            Special Occasion Package</option>
+                                        <option value="Water Bottle or Distilled Water Package"
+                                            {{ $addon->services == 'Water Bottle or Distilled Water Package' ? 'selected' : '' }}>
+                                            Water Bottle or Distilled Water Package</option>
+                                        <option value="Old Itinerary">Old Itinerary</option>
+                                        <option value="Changed Itinerary"
+                                            {{ $addon->services == 'Changed Itinerary' ? 'selected' : '' }}>Changed
+                                            Itinerary</option>
+                                    </select>
+                                </td>
+                                <td style="width:1200px">
+                                    <textarea class="form-control ckeditor" name="cruiseaddon[{{ $key }}][service_name]" rows="6">{{ $addon->service_name }}</textarea>
+                                </td>
 
-                      
-                        <td>
-                            <button type="button" class="btn btn-outline-danger delete-addon-cruise-btn">
-                                <i class="ri ri-delete-bin-line"></i>
-                            </button>
-                        </td>
-                    </tr>
-                @endforeach
-             </tbody>
-          </table>
-    </div>
-</div>
 
-<input type="hidden" id="cruise-addon-index" value="{{ isset($travel_cruise_addon) ? $travel_cruise_addon->count() : 1 }}">
-    <!------------ End Addons ------------------------------>        
+                                <td>
+                                    <button type="button" class="btn btn-outline-danger delete-addon-cruise-btn">
+                                        <i class="ri ri-delete-bin-line"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <input type="hidden" id="cruise-addon-index"
+            value="{{ isset($travel_cruise_addon) ? $travel_cruise_addon->count() : 1 }}">
+        <!------------ End Addons ------------------------------>
 
 
 
@@ -187,31 +212,31 @@
         </div>
 
         <div class="" style="margin-top:20px">
-            @if($cruise_images)
-            <table class="table table-bordered table-striped crm-table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Image Preview</th>
-                        <th>Agent Name</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($cruise_images as $key => $img)
-                    <tr>
-                        <td>{{ $key + 1 }}</td>
-                        <td><a href="{{ asset($img->file_path) }}" target="_blank"><img width="50"
-                                    src="{{ asset($img->file_path) }}" class="img-thumbnail" style="max-height: 100px;"
-                                    alt="Flight Image"></a></td>
-                        <td>{{ $img->get_agent?->name }}</td>
-                        <td>{{ $img->created_at }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            @if ($cruise_images)
+                <table class="table table-bordered table-striped crm-table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image Preview</th>
+                            <th>Agent Name</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($cruise_images as $key => $img)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td><a href="{{ asset($img->file_path) }}" target="_blank"><img width="50"
+                                            src="{{ asset($img->file_path) }}" class="img-thumbnail"
+                                            style="max-height: 100px;" alt="Flight Image"></a></td>
+                                <td>{{ $img->get_agent?->name }}</td>
+                                <td>{{ $img->created_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             @else
-            <p>No images found.</p>
+                <p>No images found.</p>
             @endif
         </div>
     </div>
