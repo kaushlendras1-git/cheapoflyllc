8e62f2f037357481444c0463d8f723a2b7edc58c
kaushlendras1-git <68238023+kaushlendras1-git@users.noreply.github.com>
2025-11-04 00:43:25 +0530

    restict ip address

--- routes/web.php
+++ routes/web.php
@@ -94,9 +94,10 @@ Route::get('/api/cruise-ports', [\App\Http\Controllers\CruisePortController::cla
 
 Route::get('/booking-information-next', function () {return view('web.booking-information-next');})->name('booking-information-next');
 
-Route::get('/', function () {return view('web.login');});
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', function () {return view('web.login');})->middleware('restrict.ip');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('restrict.ip');
+Route::post('/login', [AuthController::class, 'login'])->middleware('restrict.ip');
+
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
