cacf20b999cf026e4a9db5f6040f3a66c0ae8275
kaushlendras1-git <68238023+kaushlendras1-git@users.noreply.github.com>
2025-11-14 19:13:19 +0530

    add processing

--- resources/views/web/booking/partials/authModel.blade.php
+++ resources/views/web/booking/partials/authModel.blade.php
@@ -179,9 +179,7 @@
                         <!-- Submit Button -->
                         <div class="col-md-2 d-flex justify-content-end">
                             <button class="btn btn-primary button-style d-flex align-items-center gap-2 px-4 py-2"
-                                style="background-color: var(--primary); color: #fff !important;">
-                                <!-- <span class="iconify fs-5" data-icon="mdi:send-check-outline"
-                                    style="color: #fff !important;"></span> -->
+                                style="background-color: var(--primary); color: #fff !important;" onclick="this.disabled = true; this.innerText='Processing...';">
                                 Send Auth
                             </button>
                         </div>
